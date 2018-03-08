582be35ebbe1417645c0a581060b6f8761293dfa
Vina Compson <vv.compson@icloud.com>
2018-03-08 14:27:09 +1100

    update static components to dynamic components - nested componets like the previous commit

--- public/app.jsx
+++ public/app.jsx
@@ -1,19 +1,29 @@
 var GreetMessage = React.createClass({
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-          <h1>Some H1</h1>
-          <p>Some paragraph</p>
+          <h1>Hello {name}!</h1>
+          <p>{message}</p>
       </div>
     );
   }
 });
 
 var GreetForm = React.createClass({
+  onFormSubmit: function (e){
+    e.preventDefault();
+    var name = this.refs.name.value;
+    if(name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
   render: function (){
     return (
       <div>
-          <form>
+          <form onSubmit={this.onFormSubmit}>
             <input type="text" ref="name"/>
             <button>Set Name</button>
           </form>
@@ -35,34 +45,19 @@ var Greeter = React.createClass({
       name: this.props.name
     }
   },
-  onButtonClick: function(e) {
-    e.preventDefault();
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
-    if(typeof name === 'string' && name.length > 0){
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function(name) {
+    this.setState({
+      name: name
+    });
   },
   render: function () {
     var name = this.state.name;
     var message = this.props.message;
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message + '!!'}</p>
 
-        <GreetMessage />
-
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
-
-        <GreetForm />
+        <GreetMessage name={name} message={message}/>
+        <GreetForm onNewName={this.handleNewName}/>
 
       </div>
     );
