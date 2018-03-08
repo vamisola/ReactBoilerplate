39f503d474ba6eb21ce45551b0b4ec088ba72786
Vina Compson <vv.compson@icloud.com>
2018-03-09 08:24:27 +1100

    add simple css

--- public/app.jsx
+++ public/app.jsx
@@ -3,9 +3,13 @@ var GreetMessage = React.createClass({
     var name = this.props.name;
     var message = this.props.message;
     return (
-      <div>
-          <h1>Hello {name}!</h1>
-          <p>{message}</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 align-self-center">
+              <h1>Hello {name}!</h1>
+              <p>{message}</p>
+          </div>
+        </div>
       </div>
     );
   }
@@ -14,20 +18,36 @@ var GreetMessage = React.createClass({
 var GreetForm = React.createClass({
   onFormSubmit: function (e){
     e.preventDefault();
+
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
+
     if(name.length > 0) {
       this.refs.name.value = '';
-      this.props.onNewName(name);
+      updates.name = name;
+      //this.props.onNewName(name);
     }
+    if(message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
   },
   render: function (){
     return (
-      <div>
-          <form onSubmit={this.onFormSubmit}>
-            <input type="text" ref="name"/>
-            <button>Set Name</button>
-          </form>
-      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 align-self-center">
+              <form onSubmit={this.onFormSubmit}>
+                <div><input type="text" ref="name" placeholder="Your Name here..."/></div>
+                <div><textarea ref="message" placeholder="Your Message here..."></textarea></div>
+                <div><button>Submit</button></div>
+              </form>
+          </div>
+        </div>
+    </div>
     );
   }
 });
@@ -42,29 +62,28 @@ var Greeter = React.createClass({
   },
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     }
   },
-  handleNewName: function(name) {
-    this.setState({
-      name: name
-    });
+  handleNewData: function(updates) {
+    this.setState(updates);
   },
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
     return (
       <div>
 
         <GreetMessage name={name} message={message}/>
-        <GreetForm onNewName={this.handleNewName}/>
+        <GreetForm onNewData={this.handleNewData}/>
 
       </div>
     );
   }
 });
-
+var firstName = "Vina";
 ReactDOM.render(
-  <Greeter name="Vina" message="Message from prop"/>,
+  <Greeter name={firstName} message="Not the default message from Component."/>,
   document.getElementById("app")
 );
