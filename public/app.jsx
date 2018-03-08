6b62ab2eb696be3073801e705b85b10f7ceb3e1c
Vina Compson <vv.compson@icloud.com>
2018-03-08 13:53:28 +1100

    add state to component

--- public/app.jsx
+++ public/app.jsx
@@ -5,13 +5,24 @@ var Greeter = React.createClass({
       message: 'This is the default message.'
     }
   },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
   onButtonClick: function(e) {
     e.preventDefault();
-    var name = this.refs.name.value;
-
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+    if(typeof name === 'string' && name.length >0){
+      this.setState({
+        name: name
+      });
+    }
   },
   render: function () {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
     return (
       <div>
